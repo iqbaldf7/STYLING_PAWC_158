d5130b06b89e65941ee6c65b2706cca61ddd9b83
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-11-09 22:15:53 +0700

    dbcrud_158

--- routes/todo.js
+++ routes/todo.js
@@ -24,13 +24,13 @@ router.post('/', (req, res) => {
 
 module.exports = router;  // Menggunakan 'router', bukan 'Router'
 
+// Endpoint untuk menghapus tugas
 router.delete('/:id', (req, res) => {
-    const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
-    if (todoIndex === -1) return res.status(404).json({ message: 'Tugas tidak ditemukan' });
+    const todoIndex = todos.findIndex(t=>t.id === parseInt(req.params.id));
+    if (todoIndex === 1) return res.status(404).json({message: 'Tugas tidak ditemukan'});
 
-    const deletedTodo = todos.splice(todoIndex, 1)[0]; // Menghapus dan menyimpan todo yang dihapus
-    res.status(200).json({ message: `Tugas '${deletedTodo.task}' telah dihapus` });
+    const deletedTodo = todos.splice(todoIndex, 1)[0];  // Menghapus dan menyimpan todo yang dihapus
+    res.status(200).json({message: `Tugas '${deletedTodo.task}' telah dihapus`});
 
 });
-
 
